b78835b478f78d54413239f59533243fd61b6dd0
Danielle <daniellejazminecager@Danielles-MacBook-Air.local>
2026-04-27 23:05:38 -0500

    Add port 3000 to CORS allowed origins

--- backend/src/main/java/com/transfercreditmatch/config/SecurityConfig.java
+++ backend/src/main/java/com/transfercreditmatch/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8000", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8000", "http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
